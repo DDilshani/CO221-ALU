a46970359d79c1cf7b2240955264288a81f6aed7
NuwanJ <nuwanjaliyagoda@gmail.com>
2018-05-25 22:59:50 +0530

    compile errors fixed

--- Verilog/version1.v
+++ Verilog/version1.v
@@ -1,36 +1,38 @@
 
 module ALU;
 
-	reg [3:0]A, [3:0]B;
-	reg [2:0]C;
+	reg [3:0] A,B;
+	reg [2:0] C;
 	output Out;
 	
 	// 4bit wires
-	wire [3:0] AandB, AorB, inA inB, afterEnA, afterEnB;
+	wire [3:0] AandB, AorB, inA, inB, afterEnA, afterEnB;
 	
 	// 1bit wires
 	wire EnA, EnB, ABar, BBar, P, Q, R;
 	
-	andAB (AandB, A,B);
-	orAB (AorB, A,B);
+	andAB  myAndAB(AandB, A,B);
+	orAB myOrAB(AorB, A,B);
 	
-	moduleEnA(EnA, C[2], C[1], C[0]);
-	moduleEnB(EnB, C[2], C[1], C[0]);
-	moduleABar(ABar, C[2], C[1], C[0]);
-	moduleBBar(BBar, C[2], C[1], C[0]);
+	moduleEnA myEnA(EnA, C[2], C[1], C[0]);
+	moduleEnB myEnB(EnB, C[2], C[1], C[0]);
+	moduleABar myABar(ABar, C[2], C[1], C[0]);
+	moduleBBar myBBar(BBar, C[2], C[1], C[0]);
 	
-	modulePQRfromLMN (P,Q,R, L,M,N)
+	modulePQRfromLMN myController(P,Q,R, L,M,N);
+	
 	initial begin
 		A <= 4'b1100;
 		B <= 4'b0001;
-		Ctrl <= 3'b000; // -A
+		C <= 3'b000; // -A
 		
-		$dumpfile("alu.vcd"); 
-		$dumpvars(0, myFullAdder);
+		//$dumpfile("alu.vcd"); 
+		//$dumpvars(0, myFullAdder);
 	end
 endmodule
 
 module andAB(R, A, B);
+
     input [3:0] A, B;
     output [3:0] R;
     
@@ -43,6 +45,7 @@ module andAB(R, A, B);
 endmodule
 
 module orAB(R, A, B);
+
     input [3:0] A, B;
     output [3:0] R;
     
@@ -53,9 +56,9 @@ module orAB(R, A, B);
     
 endmodule
 
-module selectOutput(Out, P,Q, AplusB, AandB)
+//module selectOutput(Out, P,Q, AplusB, AandB)
+//endmodule
 
-endmodule
 
 //---- Control Unit ------------------------------------------------------------------
 
@@ -69,7 +72,7 @@ module moduleEnA(enA, L,M,N);
 	not b2 (notM, M);
 	
 	and b4 (out1, notL, notM, N);
-	not b5 (enA, out1)
+	not b5 (enA, out1);
 	
 endmodule 
 
@@ -103,15 +106,13 @@ module moduleABar(ABar, L,M,N);
 endmodule 
 
 module moduleBBar(BBar, L,M,N);
-	// BBar = (L'.N')
-	
-	input L, N;
+	input L, M, N;
 	output BBar;
 	wire notL, notN;
 	
+	// BBar = (L'.N')
 	not c1 (notL, L);
 	not c3 (notn, N);
-	
 	and c4 (BBar, notL, notN);
 	
 endmodule 
@@ -122,10 +123,8 @@ module moduleCin(cIn, L,M,N);
 	wire notL, notN;
 	
 	// cIn = (L'.M.N')
-	
 	not f1(notL, L);
 	not f2(notN, N);
-	
 	and f3(cIn, notL, M, notN);
 	
 endmodule 
@@ -134,18 +133,18 @@ module modulePQRfromLMN(P,Q,R, L,M,N);
 	
 	input L,M, N;
 	output P, Q, R;
-	wire xorMN, notN, 
+	wire xorMN, notN; 
 	
 	// P = L.(M (+) N)
 	// Q = L.N'
 	// R = L.M.N
 	
-	xor d1(xorMN, M,N);
-	not d2(notN, N);
+	xor d1 (xorMN, M,N);
+	not d2 (notN, N);
 	
-	and d3(P, L,xorMN);
-	and d4(Q, L, notN);
-	and d5(R, L,M,N);
+	and d3 (P, L,xorMN);
+	and d4 (Q, L, notN);
+	and d5 (R, L,M,N);
 
 endmodule
 
@@ -162,17 +161,18 @@ module oneBitFullAdder(S,cOut, A,B,cIn);
 	output S, cOut;
 	wire masterCOut, masterS, slaveCOut;
 	
-	oneBitHalfAdder masterFA(masterS,masterCOut,  A, B);
-	oneBitHalfAdder masterFA(S,slaveCOut,  A, B);
+	oneBitHalfAdder masterA(masterS,masterCOut,  A, B);
+	oneBitHalfAdder slaveA(S,slaveCOut,  masterS, cIn);
+	or (cOut, masterCOut, slaveCOut);
+	
 endmodule
 
-module oneBitHalfAdder(S,cOut, A,B,cOut);
+module oneBitHalfAdder(S,cOut, A,B);
 	input A, B, cIn;
 	output S, cOut;
 	
 	// cOut = A+B
 	// S = A (+) B
-	
 	and u1(cOut, A,B);
 	xor u2(S, A,B);
 
@@ -198,14 +198,16 @@ endmodule
 
 module busXOR(Y, X, En);
 
+	// if (EnA==1) ? Y=xor(Y, 1) : Y= Y
+	// Y[i] = X[i] (+) En
     input [3:0] X;
     input En;
     output [3:0] Y;
     
-    // if (EnA==1) ? Y=xor(Y, 1) : Y= Y
-	// Y[i] = X[i] (+) En
-	
-	
+	xor f1(Y[0], X[0], En);
+	xor f1(Y[1], X[1], En);
+	xor f1(Y[2], X[2], En);
+	xor f1(Y[3], X[3], En);
 	
 endmodule
 
